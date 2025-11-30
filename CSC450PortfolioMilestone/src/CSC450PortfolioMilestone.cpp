9d9733661de298781201f1f5e4cbdfb9aea4968c
adamgorry <adamgorry@mac.lan>
2025-11-30 04:49:29 -0700

    Initial commit, project start

---
+++ CSC450PortfolioMilestone/src/CSC450PortfolioMilestone.cpp
@@ -0,0 +1,13 @@
+//============================================================================
+// Name        : CSC450PortfolioMilestone.cpp
+// Author      : Adam Gorry
+// Version     :
+// Copyright   : Your copyright notice
+// Description : CSC450 Portfolio Milestone
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+}
