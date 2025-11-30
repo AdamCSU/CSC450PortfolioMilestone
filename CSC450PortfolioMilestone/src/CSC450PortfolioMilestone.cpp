aeb9b32ba3ec0359cec29e473fb0ea61a9efad25
adamgorry <adamgorry@mac.lan>
2025-11-30 05:49:35 -0700

    Create second thread to count down and completion message

--- CSC450PortfolioMilestone/src/CSC450PortfolioMilestone.cpp
+++ CSC450PortfolioMilestone/src/CSC450PortfolioMilestone.cpp
@@ -14,14 +14,14 @@ using namespace std;
 void countingUp() {  //Function for counting up from 1 to 20
 	for (int i = 1; i <= 20; i++) {
 		cout << "Counting up: " << i << endl;
-		this_thread::sleep_for(chrono::milliseconds(100));
+		this_thread::sleep_for(chrono::milliseconds(150));
 	}
 }
 
 void countingDown() {  //Function for counting down from 20 to 0
 	for (int i = 20; i >= 0; i--) {
 		cout << "Counting down: " << i << endl;
-		this_thread::sleep_for(chrono::milliseconds(100));
+		this_thread::sleep_for(chrono::milliseconds(150));
 	}
 }
 
@@ -32,11 +32,14 @@ int main() {
 	cout << intro << endl;
 
 	thread thread1(countingUp);  //Create first thread for counting up
-
-	thread1.join();  //Wait until thread1 finishes before continuing
+	thread1.join();  //Wait until thread1 finishes counting up
 
 	cout << "Counting down " << endl;
 
+	thread thread2(countingDown);  //Create second thread for counting down
+	thread2.join();  //Wait until thread2 finishes counting down
+
+	cout << "Both threads have completed their counting."  << endl;
 
 	return 0;
 }
